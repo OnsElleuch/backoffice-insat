e35a4007c3e4566d8fb5c3ee97c367dc3032346c
unknown <mehdi.mabrouki@koiosintelligence.ca>
2021-01-29 21:36:07 +0100

    deleted base and charts

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -51,14 +51,6 @@ export const routes: Routes = [
     },
     children: [
       {
-        path: 'base',
-        loadChildren: () => import('./views/base/base.module').then(m => m.BaseModule)
-      },
-      {
-        path: 'buttons',
-        loadChildren: () => import('./views/buttons/buttons.module').then(m => m.ButtonsModule)
-      },
-      {
         path: 'charts',
         loadChildren: () => import('./views/chartjs/chartjs.module').then(m => m.ChartJSModule)
       },
