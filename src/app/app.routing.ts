bba4e49e8032948e21beb9eb60d020407ff01a89
Sami <sami.elleuch@insat.u-carthage.tn>
2021-02-06 19:22:31 +0100

    added login and loggout Integration and added Guards

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -8,12 +8,13 @@ import { P404Component } from './views/error/404.component';
 import { P500Component } from './views/error/500.component';
 import { LoginComponent } from './views/login/login.component';
 import { RegisterComponent } from './views/register/register.component';
+import { AuthGuardService as AuthGuard } from './auth/auth-guard.service';
 
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: 'profile',
-    pathMatch: 'full',
+    component: DefaultLayoutComponent,
+    canActivate: [AuthGuard]
   },
   {
     path: '404',
@@ -53,50 +54,62 @@ export const routes: Routes = [
       {
         path: 'profile',
         loadChildren: () => import('./views/profile/profile.module').then(m => m.ProfileModule)
+        ,canActivate: [AuthGuard]
       },
       {
         path: 'news',
         loadChildren: () => import('./views/news/news.module').then(m => m.NewsModule)
+        ,canActivate: [AuthGuard]
       },
       {
         path: 'calendrier',
         loadChildren: () => import('./views/calendrier/calendrier.module').then(m => m.CalendrierModule)
+        ,canActivate: [AuthGuard]
       },
       {
         path: 'notes',
         loadChildren: () => import('./views/notes/notes.module').then(m => m.NotesModule)
+        ,canActivate: [AuthGuard]
       },
       {
         path: 'theme',
         loadChildren: () => import('./views/theme/theme.module').then(m => m.ThemeModule)
+        ,canActivate: [AuthGuard]
       },
       {
         path: 'event',
         loadChildren: () => import('./views/event/event.module').then(m => m.EventModule)
+        ,canActivate: [AuthGuard]
       },
       {
         path: 'rendez-vous',
         loadChildren: () => import('./views/rendez-vous/rendez-vous.module').then(m => m.RendezVousModule)
+        ,canActivate: [AuthGuard]
       },
       {
         path: 'direction',
         loadChildren: () => import('./views/direction/direction.module').then(m=>m.DirectionModule)
+        ,canActivate: [AuthGuard]
       },
       {
         path: 'laboratoire',
         loadChildren: () => import('./views/laboratoire/laboratoire.module').then(m=>m.LaboratoireModule)
+        ,canActivate: [AuthGuard]
       },
       {
         path: 'enseignant',
         loadChildren: () => import('./views/enseignant/enseignant.module').then(m=>m.EnseignantModule)
+        ,canActivate: [AuthGuard]
       },
       {
         path: 'partenariats',
         loadChildren: () => import('./views/partenariats/partenariats.module').then(m=>m.PartenariatsModule)
+        ,canActivate: [AuthGuard]
       },
       {
         path: 'conventions',
         loadChildren: () => import('./views/conventions/conventions.module').then(m=>m.ConventionsModule)
+        ,canActivate: [AuthGuard]
       }
     ]
   },
