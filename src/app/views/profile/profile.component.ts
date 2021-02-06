7bb102fa9041a98a449ed39fe82a7ad87c7ec7a1
Sami <sami.elleuch@insat.u-carthage.tn>
2021-02-06 21:02:03 +0100

    Implemented admin change profile integration with backend

--- src/app/views/profile/profile.component.ts
+++ src/app/views/profile/profile.component.ts
@@ -1,6 +1,11 @@
+declare var require: any;
+
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormGroup, NgForm } from '@angular/forms';
+import { SweetAlert } from 'sweetalert/typings/core';
 import { User } from '../../shared/models/user.model';
+import { ChangeInfoService } from '../../shared/services/profile.service';
+const swal: SweetAlert = require('sweetalert');
 
 @Component({
   selector: 'app-profile',
@@ -8,17 +13,21 @@ import { User } from '../../shared/models/user.model';
   styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent implements OnInit {
+  constructor( private changeInfoService: ChangeInfoService ) {}
+
   productForm: FormGroup;
   user = new User();
 
 
-  constructor() { }
-
   ngOnInit(): void {
     // add a getLoggedUser from service 
+    this.user = JSON.parse(localStorage.getItem('user'));
+
   }
-  onSubmit(){
-    console.log(this.user);
+  onSubmit(formulaire: NgForm) {
+    this.changeInfoService.changeInfo(this.user.id, formulaire.value).subscribe((res) => {
+      swal("Succès", "Profil mis ajours avec succès", "success");
+      });
   }
   uploadUserPhoto(event){
     const file = event.target.files[0];
